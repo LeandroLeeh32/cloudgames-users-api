932d719829a3f8077108ec7dc7ca8eb93a8f0e70
Luciano Miranda <luciano.miranda@viveo.com>
2026-05-19 23:19:49 -0300

    feat: ajuste cors

--- CloudGames.Users.API/Program.cs
+++ CloudGames.Users.API/Program.cs
@@ -241,9 +241,11 @@ try
     {
         c.PreSerializeFilters.Add((swagger, httpReq) =>
         {
+            var host   = httpReq.Headers["X-Forwarded-Host"].FirstOrDefault() ?? httpReq.Host.Value;
+            var scheme = httpReq.Headers["X-Forwarded-Proto"].FirstOrDefault() ?? httpReq.Scheme;
             swagger.Servers = new List<OpenApiServer>
             {
-                new() { Url = $"{httpReq.Scheme}://{httpReq.Host}/users" }
+                new() { Url = $"{scheme}://{host}/users" }
             };
         });
     });
